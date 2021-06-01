150c31ef473b44afb4f70f36bae54583b34abffc
Amir Orbe <orbebrian@gmail.com>
2021-06-01 18:00:16 -0500

    correccion de errores

--- config/config.exs
+++ config/config.exs
@@ -35,6 +35,6 @@ config :ueberauth, Ueberauth,
     github: {Ueberauth.Strategy.Github, []}
   ]
 
- config :ueberauth,  Ueberauth.Strategy.Github.Oauth,
+ config :ueberauth,  Ueberauth.Strategy.Github.OAuth,
   client_id: "e58c0877ab9a9c5c8bb6",
   client_secret: "c42c123e5f81be9a86d275bd977a3890c373184b"
