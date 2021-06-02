f439de217d777cc8e4ae79109fa380662ba6ce1d
Amir Orbe <orbebrian@gmail.com>
2021-06-01 21:52:55 -0500

    se agregaron funciones para la validacion de usuarios

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -1,13 +1,27 @@
 defmodule DiscussWeb.AuthController do
     use DiscussWeb, :controller
     plug Ueberauth
-    alias Discuss.User
+    alias DiscussWeb.User
 
     def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
         user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
         changeset = User.changeset(%User{}, user_params)
 
-        insert_or_update_user(changeset)
+        signin(conn, changeset)
+    end
+
+    defp signin(conn, changeset) do
+        case insert_or_update_user(changeset) do
+            {:ok, user} ->
+                conn
+                |> put_flash(:info, "Welcome back!")
+                |> put_session(:user_id, user.id)
+                |> redirect(to: Routes.topic_path(conn, :index)) 
+            {:error, _reason} ->
+                conn
+                |> put_flash(:error, "Error signin in")
+                |> redirect(to: Routes.topic_path(conn, :index))
+        end
     end
 
     defp insert_or_update_user(changeset) do
