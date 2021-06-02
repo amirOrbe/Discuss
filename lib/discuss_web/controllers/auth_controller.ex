1b2818c0c53697c4dbd925684daf1db219524d58
Amir Orbe <orbebrian@gmail.com>
2021-06-01 21:07:04 -0500

    se agrego codigo en callback para validar usuarios y se agrego la funcion insert_or_update... para validar el registro en la base de datos

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -1,12 +1,21 @@
 defmodule DiscussWeb.AuthController do
     use DiscussWeb, :controller
     plug Ueberauth
+    alias Discuss.User
 
-    def callback(conn, params) do
-        IO.puts "++++++++++++"
-        IO.inspect(conn.assings)
-        IO.puts "++++++++++++"
-        IO.inspect(params)
-        IO.puts "++++++++++++"
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+        changeset = User.changeset(%User{}, user_params)
+
+        insert_or_update_user(changeset)
+    end
+
+    defp insert_or_update_user(changeset) do
+        case Repo.get_by(User, email: changeset.changes.email) do
+            nil -> 
+                Repo.insert(changeset)
+            user ->
+                {:ok, user}
+        end
     end
 end
