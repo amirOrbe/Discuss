22cdd685ddeb41d09c711befa07bf3c740092bb4
Amir Orbe <orbebrian@gmail.com>
2021-06-08 16:02:16 -0500

    se agrego al modelo los comentarios

--- lib/discuss_web/topic.ex
+++ lib/discuss_web/topic.ex
@@ -5,6 +5,7 @@ defmodule DiscussWeb.Topic do
     schema "topicss" do
         field :title, :string
         belongs_to :user, DiscussWeb.User
+        has_many :comments, DiscussWeb.Comment
     end
 
     def changeset(struct, params \\ %{}) do
